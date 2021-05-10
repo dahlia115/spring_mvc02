48d5ed092ea0186ae72bdcd551e25af40c821ec2
jeongwoo <dahlia11444@gmail.com>
2021-05-10 17:48:27 +0900

    이전에사용하던방식

--- src/main/java/com/care/di_ex01/MemberController.java
+++ src/main/java/com/care/di_ex01/MemberController.java
@@ -1,5 +1,20 @@
 package com.care.di_ex01;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller		//context:component-scan에 등록 해야한다.
+public class MemberController {
+	MemberService ms;
+	public MemberController() {
+		System.out.println("MemberController 실행");
+	}
+	@RequestMapping("insert")
+	public String insert(Model model) {
+		model.addAttribute("test","연습중");
+		ms = new MemberService();
+		ms.insert(model);
+		return "di/index";
+	}
 }
