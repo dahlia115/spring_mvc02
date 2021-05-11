34889d805326a8edd49cac09d33b6a9b71ea68c5
jeongwoo <dahlia11444@gmail.com>
2021-05-11 17:19:30 +0900

    redirect 연습

--- src/main/java/com/care/root/HomeController.java
+++ src/main/java/com/care/root/HomeController.java
@@ -4,10 +4,14 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,5 +39,38 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+	@GetMapping("re/index")
+	public String index() {
+		return "re/index";
+	}
+	@RequestMapping("re/result")
+	public String result(HttpServletRequest request) {
+		String id = request.getParameter("id");
+		System.out.println(id);
+		logger.info("------"+id+"------");
+		if(id.equals("abc")) {
+			return "redirect:rsOK";
+		}
+		return "redirect:rsNO"; //redirect 컨트롤러로 연결한다.
+	}
+	@RequestMapping("re/rsOK")
+	public String rsOK() {
+		return "re/rsOK";
+	}
+	@RequestMapping("re/rsNO")
+	public String rsNO() {
+		return "re/rsNO";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
